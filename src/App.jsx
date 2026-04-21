e2e1cb330a373aa7cb38da7bfc00639a2631763f
Sarin <sarinms777@gmail.com>
2026-04-21 13:51:26 +0530

    feat: add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
-
-function App() {
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Redirect from "./pages/Redirect";
+ 
+export default function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/:code" element={<Redirect />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+ 
